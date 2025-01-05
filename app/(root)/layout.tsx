890dd01656db65ef25d8358c79174d2a6dae1bc5
NyLaurent <laulanyumbayire@gmail.com>
2025-01-05 14:52:53 -0600

    changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({children}:Readonly<{children: React.ReactNode}>)
 
     return(
         <main className="font-work-sans">
-<Navbar/>
+    <Navbar/>
             {children}
         </main>
     )
