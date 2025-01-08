f53d16f6a37630c41b88838995b5be05506bcde5
NyLaurent <laulanyumbayire@gmail.com>
2025-01-08 06:28:21 -0600

    search functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
 import Image from "next/image";
+import SearchForm from "../components/SearchForm";
 export default function Home() {
   return (
     <>
-    <h1 className="heading">Home</h1>
+
+
+    <section className="pink_container">
+    <h1 className="heading">Pitch Your Startup, <br />
+    Connect with Entreprenuers</h1>
+    <p className="sub-heading !max-w-3xl">
+      Submit ideas, Vote on Pitches, and Get Noticed in 
+      Virtual Competitions.
+
+    </p>
+    <SearchForm/>
+
+    </section>
+    
     
     </>
   );
