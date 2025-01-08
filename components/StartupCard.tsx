9ecd5e7fbb417ea53f6d2bba13f6cf149e5791d8
NyLaurent <laulanyumbayire@gmail.com>
2025-01-08 11:50:18 -0600

    sanity configured

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
+import { Button } from './ui/button'
 import Image  from 'next/image'
 import { formatDate } from '@/lib/utils'
 import { EyeIcon } from 'lucide-react'
 
 const StartupCard = ({post}:{post: StartupTypeCard}) => {
 
-    const {_createdAt,views,author:{authorId,name},title,category,_id,image}=post;
+    const {_createdAt,views,author:{authorId,name},title,category,_id,image,
+description}=post;
   return (
     <li className='startup-card group'>
         <div className='flex-between'>
@@ -38,6 +40,25 @@ const StartupCard = ({post}:{post: StartupTypeCard}) => {
             width={48}
             height={48}
             className='rounded-full'></Image></Link>
+        </div>
+
+
+        <Link href={`/startup/${_id}`}>
+        <p className='startup-card_desc'>
+            {description}
+        </p>
+        <img src={image} alt="" className='startup-card_img' />
+        
+        </Link>
+        <div className='flex-between gap-3 mt-5'>
+            <Link href={`/?query=${category.toLowerCase()}`}>
+            <p className='text-16-medium'>{category}</p>
+            
+            </Link>
+            <Button className='startup-card_btn' asChild>
+                <Link href={`/startup/${_id}`}>
+                Details</Link>
+            </Button>
         </div>
     </li>
   )
