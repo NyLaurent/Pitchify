9ae62ffd7a519dff3bc38217dacdc010947275bf
NyLaurent <laulanyumbayire@gmail.com>
2025-01-11 09:20:24 -0600

    caching error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,7 @@ import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch,SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 export default async function Home({
   searchParams
@@ -11,7 +12,11 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query;
-  const params = {search:query || null}
+  const params = {search:query || null};
+
+
+  const session = await auth();
+  console.log(session?.id);
 
 
   const {data:posts}= await sanityFetch( {query: STARTUPS_QUERY,params});
