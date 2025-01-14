6ff1ce09812a778e6013e92be08f7b6f057b0e5a
NyLaurent <laulanyumbayire@gmail.com>
2025-01-14 05:30:19 -0600

    finished the startup form

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -3,6 +3,7 @@
 
 import React, { useState } from 'react'
 import { Input } from './ui/input'
+import { Textarea } from './ui/textarea'
 
 
 
@@ -25,6 +26,48 @@ const StartupForm = () => {
         />
         {errors.title && <p className='startup-form_error'>
             {errors.title}</p>}
+    </div>
+    <div>
+        <label htmlFor="description" className='startup-form_label'>
+           Description
+        </label>
+        <Textarea
+        id='description'
+        name='description'
+        className='startup-form_textarea'
+        required
+        placeholder='Startup Description'
+        />
+        {errors.description && <p className='startup-form_error'>
+            {errors.description}</p>}
+    </div>
+    <div>
+        <label htmlFor="category" className='startup-form_label'>
+           Category
+        </label>
+        <Input
+        id='title'
+        name='title'
+        className='startup-form_input'
+        required
+        placeholder='Startup Category (Tech, Health, Education...)'
+        />
+        {errors.category && <p className='startup-form_error'>
+            {errors.category}</p>}
+    </div>
+    <div>
+        <label htmlFor="link" className='startup-form_label'>
+          Image URL
+        </label>
+        <Input
+        id='link'
+        name='link'
+        className='startup-form_input'
+        required
+        placeholder='Startup Image URL'
+        />
+        {errors.link && <p className='startup-form_error'>
+            {errors.link}</p>}
     </div>
    </form>
   )
