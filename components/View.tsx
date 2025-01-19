4cca5667db7a0360383357fa6176da0ee46372f8
NyLaurent <laulanyumbayire@gmail.com>
2025-01-18 20:06:07 -0600

    debugged

--- components/View.tsx
+++ components/View.tsx
@@ -1,45 +1,32 @@
-import React from 'react'
-import Ping from './Ping'
-import { client } from '@/sanity/lib/client'
-import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
-import { writeClient } from '@/sanity/lib/write-client'
-import { cookies } from 'next/headers'
+import Ping from "@/components/Ping";
+import { client } from "@/sanity/lib/client";
+import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
+import { unstable_after as after } from "next/server";
 
-const View = async ({id}: {id: string}) => {
-    // Get the current views
-    const {views: totalViews} = await client.withConfig({useCdn: false}).fetch(STARTUP_VIEWS_QUERY, {id})
-    
-    // Get cookies instance
-    const cookieStore = await cookies()
-    const viewCookie = cookieStore.get(`view-${id}`)
-    
-    if (!viewCookie) {
-        try {
-            await writeClient
-                .patch(id)
-                .set({views: totalViews + 1})
-                .commit()
-                
-            // Set cookie with the resolved cookieStore
-            cookieStore.set(`view-${id}`, 'true', {
-                httpOnly: true,
-                maxAge: 60 * 60 * 24 // 24 hours
-            })
-        } catch (error) {
-            console.error('Error updating view count:', error)
-        }
-    }
+const View = async ({ id }: { id: string }) => {
+  const { views: totalViews } = await client
+    .withConfig({ useCdn: false })
+    .fetch(STARTUP_VIEWS_QUERY, { id });
 
-    return (
-        <div className='view-container'>
-            <div className='absolute -top-2 -right-2'>
-                <Ping/>
-            </div>
-            <p className='view-text'>
-                <span className='font-black'>Views: {totalViews}</span>
-            </p>
-        </div>
-    )
-}
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit(),
+  );
 
-export default View
+  return (
+    <div className="view-container">
+      <div className="absolute -top-2 -right-2">
+        <Ping />
+      </div>
+
+      <p className="view-text">
+        <span className="font-black">Views: {totalViews}</span>
+      </p>
+    </div>
+  );
+};
+export default View;
